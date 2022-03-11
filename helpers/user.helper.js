89877f1a3845f38c91334e2fc42b8e981f1259e2
Iryna Vakulovska <vakulovskaya@gmail.com>
2022-03-11 12:33:33 -0800

    added new tests for user login

--- helpers/user.helper.js
+++ helpers/user.helper.js
@@ -5,18 +5,30 @@ import supertest from 'supertest';
 //  Import dotenv package - to work with environmental project's variables
 import 'dotenv/config';
     //Create a new Class for authHelper - will store response from a server in response property (variable) all the methods (functions) that related to auth
-class AuthHelper {
+class UserHelper {
   constructor() {
     this.response = null;
   }
     //Create a new method
-  async login(userName, userPassword) {
+  async create(firstName, lastName, email, password) {
     //Create, setup, send request to server, wait for the response (async/await) and save the respponse from server to response property (variable)
     await supertest(process.env.BASE_URL)
       //Setup a request method - POST and an endpoint - /auth
-      .post('/auth')
+      .post('/user')
       //Setup payload - object with 2 keys - login and password (and their values)
-      .send({ login: userName, password: userPassword })
+      .send({ firstName, lastName, email, password, agreement:true })
+      //Save a response from server to esponse property (variable)
+      .then((res) => {
+        this.response = res;
+      });
+  }
+  async login(email, password) {
+    //Create, setup, send request to server, wait for the response (async/await) and save the respponse from server to response property (variable)
+    await supertest(process.env.BASE_URL)// create request object to server
+      //Setup a request method - POST and an endpoint - /auth
+      .post('/user/login')
+      //Setup payload - object with 2 keys - login and password (and their values)
+      .send({email, password})
       //Save a response from server to esponse property (variable)
       .then((res) => {
         this.response = res;
@@ -24,5 +36,5 @@ class AuthHelper {
   }
 }
     //Export the Class
-export default AuthHelper;
+export default UserHelper;
 
---
+++ specs/user.spec.js
@@ -0,0 +1,104 @@
+//IMPORT SECTION
+//  Import {expect} assertion function from Chai assertion library
+import { expect } from "chai";
+//  Import helper(s)
+import UserHelper from "../helpers/user.helper";
+//  Creating a new instance of helper(s)
+const userHelper = new UserHelper();
+//  Main Test Suite
+describe.skip("\nSuccessful create new user sub suite (happy path with valid login and password)", () => {
+  //BEFORE hook
+  before(async () => {
+    await userHelper.create("firstName", "lastName", process.env.EMAIL, process.env.PASSWORD);
+    console.log(userHelper.response);
+  });
+  //Test Cases
+  it("Checking that response status code is 201", () => {
+    expect(userHelper.response.statusCode).to.eq(201);
+  });
+  //
+  it("Checking that response include message", () => {
+    expect(userHelper.response.body.message).to.eq("User created successfully. Please check your email and verify it");
+  });
+  it("Checking that response include message", () => {
+    expect(userHelper.response.body.message).not.to.be.undefined;
+  });
+  //
+  it("Checking that response include success", () => {
+    expect(userHelper.response.body.success).to.eq(true);
+  });
+  it("Checking that response include success", () => {
+    expect(userHelper.response.body.success).not.to.be.undefined;
+  });
+  //
+  it("Checking that response include fail", () => {
+    expect(userHelper.response.body.fail).to.eq(false);
+  });
+  it("Checking that response include fail", () => {
+    expect(userHelper.response.body.fail).not.to.be.undefined;
+  });
+  //
+  it("Checking that response include silent", () => {
+    expect(userHelper.response.body.silent).to.eq(true);
+  });
+  it("Checking that response include silent", () => {
+    expect(userHelper.response.body.silent).not.to.be.undefined;
+  });
+});
+//Login
+describe("\nSuccessful login user sub suite (happy path with valid login and password)", () => {
+  //BEFORE hook
+  before(async () => {
+    await userHelper.login(process.env.EMAIL, process.env.PASSWORD);
+    process.env.USERID = userHelper.response.body.payload.userId;
+    console.log(process.env.USERID);
+  });
+  //Test Cases
+  it("Checking that response status code is 200", () => {
+    expect(userHelper.response.statusCode).to.eq(200);
+  });
+  //
+  it("Checking that response include message", () => {
+    expect(userHelper.response.body.message).to.eq("Auth success");
+  });
+  it("Checking that response include message", () => {
+    expect(userHelper.response.body.message).not.to.be.undefined;
+  });
+  //
+  it("Checking that response include token with length has more than 200 symbols", () => {
+    expect(userHelper.response.body.payload.token).to.have.lengthOf.greaterThan(200);
+  });
+  it("Checking that response include token", () => {
+    expect(userHelper.response.body.payload.token).not.to.be.undefined;
+  });
+  //
+  it("Checking that response.body.payload.token is a string", () => {
+    expect(userHelper.response.body.payload.token).to.be.an("string");
+  });
+  it("Checking that response include user role is array", () => {
+    expect(userHelper.response.body.payload.user.roles).to.be.an("array");
+  });
+  //
+  it("Checking that response body.payload.user.roles", () => {
+    expect(userHelper.response.body.payload.user.roles).not.to.be.undefined;
+  });
+  it("Checking that response include user role is new", () => {
+    expect(userHelper.response.body.payload.user.roles[0]).to.be.eq("new");
+  });
+  it("Checking that response includs acl key that is not undefined", () => {
+    expect(userHelper.response.body.payload.acl).not.to.be.undefined;
+  });
+  it("Checking that response include acl is user.auth", () => {
+    expect(userHelper.response.body.payload.acl[0]).to.be.eq("user.auth");
+  });
+  it("Checking that response include userID", () => {
+    expect(userHelper.response.body.payload.userId).not.to.be.undefined;
+  });
+  //
+  it("Checking that response.body.payload.token is a string", () => {
+    expect(userHelper.response.body.payload.userId).to.be.an("string");
+  });
+  it("Checking that response include userID has 24 symbols", () => {
+    expect(userHelper.response.body.payload.userId.length).to.be.eq(24);
+  });
+});
